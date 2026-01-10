99598416e7231f2ab07d1b77f9f1c230e97ac5b3
jack_hill <82217588+jackhill568@users.noreply.github.com>
2026-01-10 06:24:05 +0000

    Delete lua directory

--- lua/extras/lazyExtras.lua
+++
@@ -1,8 +0,0 @@
-return {
-
-    { import = "lazyvim.plugins.extras.ui.mini-starter" },
-    { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.java" },
-    { import = "lazyvim.plugins.extras.lang.tex" },
-}
